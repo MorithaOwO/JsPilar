15fa6c3db8cfeea68695a0dcb8702e9503acc7f9
morithaowo <lkliss.yasha@gmail.com>
2023-10-01 22:04:55 -0300

    Ejercicios de Variables terminado

---
+++ Clase (1) 15-09-23/variables/index.js
@@ -0,0 +1,33 @@
+//Ejercicio 1 🌸
+
+// const firstname = "Katsura"
+// const username = "PanConKesito"
+// const age = 26
+// const email= "PanConKesito@gmail.com"
+// console.log(typeof firstname)
+// console.log(typeof age)//
+
+//Ejercicio 2 🌸
+
+// const username= prompt ("Ingresa tu NOMBRE:")
+// const age= prompt ("Ingresa tu edad:")
+// const contry= prompt ("Ingresa tu país:")
+// const profession= prompt("Ingresa tu profesión:")
+// console.log(`El usuario ingresado es ${username}`)
+// console.log(`La edad ingresada es ${age}`)
+// console.log(Number(age)+10)
+// console.log(`El país ingresado es ${contry}`)
+// console.log(`La profesión ingresada es ${profession}`)
+
+//Ejercicio 3 🌸
+
+// document.write(`Nombre:${firstname}, User: ${username}, Edad: ${age}`)
+// alert('No te olvides de responder las siguientes preguntas')
+
+//Ejercicio 4 🌸
+
+const number1= prompt('Ingrese un numero:')
+const number2= prompt('Ingrese tu segundo numero')
+const resultado= Number(number1) + Number(number2)
+console.log(resultado)
+document.write(`El resultado de la suma es: ${resultado}`)
